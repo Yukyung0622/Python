053d3aaa5f32937bc7657723ffe653780ee0b897
unknown <kkomang0622@gmail.com>
2022-01-05 10:41:29 +0900

    2022/01/04 교재예제

--- Study/Ch02/p54.py
+++ Study/Ch02/p54.py
@@ -3,4 +3,10 @@ print('escape 문자 차단')
 print('\n출력 이스케이프 문자') #\n : 줄바꿈기능
 
 #escape 문자기능차단
-print('\n')
+print('\\n출ㄹ겨 이스케이프 기능 차단1')
+print(r'\n출력 이스케이프 기능 차단2')
+
+#경로 표현 : C:\Python\test
+print('path = ', 'C:\python\test')
+print('path = ', 'C:\python\\test')
+print('path = ', r'C:\python\test')
